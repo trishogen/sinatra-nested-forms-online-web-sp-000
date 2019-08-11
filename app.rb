99c8236f02f58a6bf9369245bf59383871ed45c0
Trish Ogen <patriciaogen@gmail.com>
2019-08-11 02:28:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,11 +13,12 @@ module FormsLab
 
     post '/pirates' do
       @pirate = Pirate.new(params['pirate']['name'], params['pirate']['weight'], params['pirate']['height'])
+
       params[:pirate][:ships].each do |ship|
         Ship.new(ship['name'], ship['type'], ship['booty'])
       end
 
-        @ships =  Ship.all
+      @ships = Ship.all
 
       erb :'pirates/show'
     end
