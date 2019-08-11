9f64031c1c54f65f12be8784481b2ee2925f0838
Trish Ogen <patriciaogen@gmail.com>
2019-08-11 01:52:50 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,10 +1,16 @@
 class Pirate
   attr_accessor :name, :weight, :height
 
+  @@pirates = []
+  
   def initialize(name, weight, height)
     @name = name
     @weight = weight
     @height = height
+    @@pirates << self
   end
-  
+
+  def self.all
+    @@pirates
+  end
 end
