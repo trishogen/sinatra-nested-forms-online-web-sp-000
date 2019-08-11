3ccf0a3cd48322ef519388ea67e6546a7afa6c4b
Trish Ogen <patriciaogen@gmail.com>
2019-08-11 02:16:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ module FormsLab
     end
 
     post '/pirates' do
-      @pirate = Pirate.new(params['pirate'])
+      #@pirate = Pirate.new(params['pirate'])
       erb :'pirates/shows'
     end
 
