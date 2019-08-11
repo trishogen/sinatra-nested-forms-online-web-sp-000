8beab6d14b6afc3e561493554fde76c28c529273
Trish Ogen <patriciaogen@gmail.com>
2019-08-11 02:32:47 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@pirates = []
+
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@ships = []
+
+  def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships.clear
+  end
 end
